701f99b63c9c086b1e5d15c1ca8aff59cd6ec376
yuuutong <97610255+yuuutong@users.noreply.github.com>
2022-07-18 11:28:20 +0800

    sidebar colour

--- sleeplah/lib/constant.dart
+++ sleeplah/lib/constant.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const primaryColor = Color.fromARGB(255, 147, 184, 248);
 const themePrimaryColor = Color.fromARGB(255, 123, 170, 251);
 const themeSecondaryColor = Color.fromARGB(255, 174, 245, 254);
-const darkColor = Colors.blue;
+const darkColor = Color.fromARGB(255, 191, 205, 247);
 const String flower_profile_path = "assets/images/";
 String date =
     "${DateTime.now().year}-${DateTime.now().month.toString().padLeft(2, '0')}-${DateTime.now().day.toString().padLeft(2, '0')}";
